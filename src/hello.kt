1c14028c3f3e64e740cda2d5939c3f8cbbcdadfb
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2020-11-30 14:35:27 -0300

    btw: var is mutable, val is immutable and Kotlin can infer types

--- src/hello.kt
+++ src/hello.kt
@@ -9,6 +9,16 @@ fun laugh() {
 
 
 fun main () {
-    greetKotlin("Kotlin")
-    laugh()
+//    greetKotlin("Kotlin")
+//    laugh()
+
+    // var is mutable
+    // val is immutable
+
+    var x1: Int = 1
+    val x2: Int = 2
+    val x3 = 3
+
+    println("$x1 $x2 $x3")
+
 }
