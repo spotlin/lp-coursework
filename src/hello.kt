218f8b0a5a79e4ac530f2f12c9bb53cc1aa70e6a
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2020-11-30 14:22:52 -0300

    chore: add infix extension function on Int (Int.times)

--- src/hello.kt
+++ src/hello.kt
@@ -2,6 +2,13 @@ fun greetKotlin(name: String, greetingMsg: String = "Welcome") {
     println("$greetingMsg, $name");
 }
 
+fun laugh() {
+    infix fun Int.times(str: String) = str.repeat(this)
+    println(10 times "k")
+}
+
+
 fun main () {
     greetKotlin("Kotlin")
+    laugh()
 }
