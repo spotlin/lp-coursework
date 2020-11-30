03c2a2f6680efff70077c6b1fd6b30caa248a3c4
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2020-11-30 16:06:42 -0300

    chore: create class; btw: the header of a class declaration is also its constructor

--- src/hello.kt
+++ src/hello.kt
@@ -16,8 +16,14 @@ fun strLength(str: String): Int {                   // 7
     return str.length
 }
 
+class User (var id: Int, var email: String)
+
 fun main () {
 //    greetKotlin("Kotlin")
 //    laugh()
-//    measureStr(("Ok Kotlin"))
+//    measureStr(("Ok Kotlin")
+
+    val user1 = User(1, "dan@dan.com")
+    println("${user1.id}: ${user1.email}")
+
 }
